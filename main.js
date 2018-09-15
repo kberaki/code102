659c9f8817955a34bb6d3b026535fd07b6e0ed9c
kberaki <berakik@gmail.com>
2018-09-15 11:31:20 -0400

    adding my JS file

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+let box= "bottle";
+console.log("box = " + box);
+ box = "phone";
+console.log("box = " + box);
+box = "wire";
+console.log("box = " + box);
+
+let city = "Asmara";
+console.log(city);
+city = "Washington DC";
+console.log(city);
+
+let x = "java";
+console.log(x);
+let b = "java";
+console.log(x == b);
+let equal = (x == b);
+console.log(x == b);
+
+
