8346ca8480d492e73d78c8e97bac6f647aff3a48
kberaki <berakik@gmail.com>
2018-09-15 14:16:41 -0400

    adding etery file

--- main.js
+++ main.js
@@ -18,4 +18,19 @@ console.log(x == b);
 let equal = (x == b);
 console.log(x == b);
 
-
+ let speed = 50;
+ if (speed<30){
+     console.log("No ticket")
+ }
+ else if(speed<=40){
+ console.log("$50 fine!")
+ }
+ else {
+     console.log("$100 fine")
+ }
+let pro = prompt(" Welcome to my Page!");
+console.log(pro);
+let con;
+con = confirm("This is a thank you page, are you thank full!")
+let al = alert("I am loving this!")
+console.log(al);
